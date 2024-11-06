85f51864901b5347f4df48ee405c91cb4037bd62
JimcostDev <rjak727@gmail.com>
2024-11-06 14:33:08 +0100

    primeros pasos, aprendiendo sobre unit testing

---
+++ tests/tests_calc.py
@@ -0,0 +1,16 @@
+import unittest
+
+from src.calculator import sum, subtract
+
+# usando unittest
+class CalculatorTests(unittest.TestCase):
+
+    def test_sum(self):
+        assert sum(2, 3) == 5
+
+    def test_subtract(self):
+        assert subtract(10, 5) == 5
+        
+# usando pytest
+def test_suma(): 
+    assert sum(4, 3) == 7
