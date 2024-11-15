54350d882c46f84b9a4e48302aa02f1a11bc49b1
JimcostDev <rjak727@gmail.com>
2024-11-15 13:06:32 +0100

    elimine test sync, uso de skip para probar mock

--- tests/api/test_perfil.py
+++ tests/api/test_perfil.py
@@ -3,6 +3,7 @@ import httpx
 from unittest.mock import patch
 
 @pytest.mark.asyncio
+@pytest.mark.skip(reason="ver tiempo de ejecución usando mock") 
 async def test_perfil_endpoint():
     url = "https://jimcostdev.koyeb.app/perfil/jimcostdev"
     
@@ -36,41 +37,7 @@ async def test_perfil_endpoint():
         "id": 1
     }
     assert response.json() == expected_response
-    
-def test_perfil_endpoint_sync():
-    url = "https://jimcostdev.koyeb.app/perfil/jimcostdev"
-    
-    # Realizar la solicitud HTTP usando un cliente síncrono
-    with httpx.Client() as client:
-        response = client.get(url)
-    
-    # Verificar que la respuesta tenga un código de estado 200
-    assert response.status_code == 200
-
-    # Verificar que la respuesta tenga el contenido esperado
-    expected_response = {
-        "rol": "Software Engineer",
-        "description": "Soy Ingeniero Informático con sólida experiencia en el desarrollo de software y un enfoque especializado en desarrollo web utilizando Python y JavaScript. A lo largo de mi carrera, me he destacado no solo por mis habilidades técnicas, sino también por mi capacidad para enseñar, lo que me ha permitido ejercer como docente y tutor de programación, acompañando a estudiantes en su formación.\n\nAdemás, cuento con certificaciones como: MongoDB Associate Developer, Associate DBA y Microsoft Azure Fundamentals, lo que respalda mis conocimientos en tecnologías de bases de datos y entornos en la nube.",
-        "skills": [
-            "python",
-            "javascript",
-            "go",
-            "fastapi",
-            "django",
-            "tailwind",
-            "astro",
-            "react",
-            "mongodb",
-            "postgresql",
-            "sqlserver",
-            "git"
-        ],
-        "username": "jimcostdev",
-        "avatar": "https://avatars.githubusercontent.com/u/53100460?v=4",
-        "id": 1
-    }
-    assert response.json() == expected_response
-
+ 
 # Usa pytest-mock o el decorador patch de unittest para simular la solicitud
 @patch("httpx.Client.get")
 def test_perfil_endpoint_mock(mock_get):
@@ -95,4 +62,3 @@ def test_perfil_endpoint_mock(mock_get):
     # Comprueba que los datos sean correctos
     assert response.status_code == 200
     assert response.json()["username"] == "jimcostdev"
-    assert response.json() == mock_response.json()
